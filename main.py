06476142c9aacef3bf6cf87cc958e2c983c4b37f
Ghost <ghostgamesv11@gmail.com>
2021-02-23 14:56:07 +0100

    lab1

--- main.py
+++ main.py
@@ -1,3 +1,67 @@
+#zad1
+# napis = 'Ala'
+# a = 5
+# b = 3.26
+# c = 3+5j
+#
+# print(a,b,c,napis)
 
-a = 2
-print(a)
+# #zad2
+# print('1.Dodawanie')
+# print('2.Odejmowanie')
+# print('3.Mnożenie')
+# print('4.Dzielenie')
+# dzialanie = input('wprowadz rodzaj dzialania:dodawanie/odejmowanie/mnozenie/dzielenie: ')
+#
+# a = float(input('Podaj pierwsza lcizbe: '))
+# b = float(input('Podaj druga liczbe: '))
+# if  dzialanie == '1':
+#             print(a,'+',b,'=',a+b)
+# elif dzialanie == '2':
+#             print(a, '-', b, '=', a - b)
+# elif dzialanie == '3':
+#             print(a, '*', b, '=', a * b)
+# elif dzialanie == '4':
+#             print(a, '/', b, '=', a / b)
+
+
+#zad4
+# import math
+# #1
+# print(math.e**10)
+# #2
+# x =math.sin(8)**2
+# print(math.pow(x, 1.0/6))
+# #3
+# print(math.floor(3.55))
+# #4
+# print(math.ceil(4.80))
+
+
+# #zad5
+#imie = "BARTOSZ"
+#nazwisko = "OKONSKI"
+#print(str.capitalize(imie) + " " + str.capitalize(nazwisko))
+
+# #zad6
+# tekst = 'Im covering my ears like a kid When your words mean nothing, I go la la la Im turning up the volume when you speak Cause if my heart cant stop it, I find a way to block it, I go La la, la la la la la na na na na na La la na na, la la la la la na na na na na'
+# x = tekst.count('la')
+# print(x)
+
+#zad7
+#imie = 'Bartosz'
+#print(imie[1])
+#print(imie[6])
+
+
+#zad 8
+
+#tekst = 'Im covering my ears like a kid When your words mean nothing, I go la la la Im turning up the volume when you speak Cause if my heart cant stop it, I find a way to block it, I go La la, la la la la la na na na na na La la na na, la la la la la na na na na na'
+#x = tekst.split()
+#print(x)
+
+#zad9
+#a = 435
+#b = 'to jest'
+#c = hex(a)
+#print(a,b,c)
